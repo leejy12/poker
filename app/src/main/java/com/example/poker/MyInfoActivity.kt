10e0ab6904d18b5db5c73f94bd492985b95aa7d9
leejy12 <leejy31415@gmail.com>
2022-07-16 00:44:20 +0900

    get myinfo

--- app/src/main/java/com/example/poker/MyInfoActivity.kt
+++ app/src/main/java/com/example/poker/MyInfoActivity.kt
@@ -2,29 +2,36 @@ package com.example.poker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.poker.Data.ResponseType
-import com.example.poker.Data.User
-import java.io.IOException
+import android.util.Log
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class MyInfoActivity : AppCompatActivity() {
-    lateinit var username: String
-    private var user: User? = null
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_info)
+
+        val nameTextView = findViewById<TextView>(R.id.name)
+        val chipCountTextView = findViewById<TextView>(R.id.chipCount)
+        val bestWinTextView = findViewById<TextView>(R.id.bestWin)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "${IP.getIP()}/player/name/${Global.currentPlayerName}"
+
+        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET,
+            url,
+            null,
+            { response ->
+                nameTextView.text = response.getJSONObject(0).getString("name")
+                chipCountTextView.text = response.getJSONObject(0).getString("chip_count")
+                bestWinTextView.text = response.getJSONObject(0).getString("best_win")
+            },
+            { error -> Log.d("why?", error.message.toString())}
+        )
+
+        jsonArrayRequest.setShouldCache(false)
+        queue.add(jsonArrayRequest)
     }
-
-    private fun initUser() {
-        username = intent.getStringExtra("id").toString()
-//        Log.d(TAG, "$userId")
-
-        getUser(username!!)
-    }
-
-    private fun getUser(id: String) {
-
-    }
-
 }
