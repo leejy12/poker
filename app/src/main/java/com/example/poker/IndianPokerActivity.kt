991de1c6e0e45acd023245949fbf409140ae77f0
leejy12 <leejy31415@gmail.com>
2022-07-16 14:47:48 +0900

    change plan

---
+++ app/src/main/java/com/example/poker/IndianPokerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.poker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndianPokerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_indian_poker)
+    }
+}
--- app/src/main/java/com/example/poker/Table.kt
+++
@@ -1,3 +0,0 @@
-package com.example.poker
-
-class Table(val tableId: String, val tableName: String, val maxPlayers: Int, val currentPlayers: Int)
